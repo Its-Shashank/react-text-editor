a8001b53649ba0126d937d80c03a4e1db6784026
Shashank Gaur <gaurshashank014@gmail.com>
2020-09-22 10:54:34 +0530

    running with some styles

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-// import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import 'react-web-tabs/dist/react-web-tabs.css';
 
 ReactDOM.render(
